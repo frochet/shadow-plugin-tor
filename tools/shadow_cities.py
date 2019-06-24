b000def6eff8260e411c6da8150cdb7ba1d05941
Florentin Rochet <frochet@oscar.elen.ucl.ac.be>
2019-06-24 13:56:21 +0200

    add pmatrix calculation

--- tools/shadow_cities.py
+++ tools/shadow_cities.py
@@ -25,6 +25,11 @@ class City:
         self.lat = lat
         self.long = longit
 
+class RipeProbe:
+    def __init__(self, ip, city):
+        self.ip = ip
+        self.city = city
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--shadowtopology")
@@ -33,32 +38,59 @@ parser.add_argument("--cityinfo")
 def main(args):
     
     cities = {}
+    ripe_probes = {}
+    pmatrix = {}
     with open(args.shadowtopology) as f:
         line = f.readline()
         while line:
             if "node id" in line: # start to parse a node!
-                f.readline() #skip ip
+                line = f.readline()
+                ip =  line.split('<data key="d1">')[1].split("</data>")[0] # location info
                 line = f.readline()
                 if "d2" in line:
-                    location = int(line.strip('<data key="d2"/>\n')) # location info
+                    location = int(line.split('<data key="d2">')[1].split("</data>")[0]) # location info
                 else:
                     continue
-                countrycode = f.readline().strip('<data key="d3"/>\n')
+                countrycode = int(f.readline().split('<data key="d3">')[1].split("</data>")[0])
+                city = City(location, countrycode, None, None, None)
                 if location not in cities:
-                    cities[location] = City(location, countrycode, None, None, None)
+                    cities[location] = city 
+                ripe_probes[ip] = RipeProbe(ip, city)
+            elif "edge source" in line: #building the penalty matrix
+		# first parse info
+                tab = line.split(" ")
+                source = tab[1].split("source=")[1]
+                dest = tab[2].split("target=")[1]
+                line = f.readline() #latency
+                if "d6" in line:
+                    latency = float(line.split('<data key="d6">')[1].split("</data>")[0])
+                else:
+                    print("No latency information for this edge?")
+                line = f.readline() #loss
+                if "d7" in line:
+                    loss = float(line.split('<data key="d6">')[1].split("</data>")[0])	
+		
+                if source not in pmatrix:
+                    pmatrix[source] = {dest: latency*loss}
+                else:
+                    pmatrix[source][dest] = latency*loss
+
             line = f.readline()
+		
     
+    ## Note: doesn't look like the city code match. What is shadow's city code?
     xls_population = pd.ExcelFile(args.cityinfo)
     cityinfo = xls_population.parse(0, header=16)
-    pdb.set_trace()
+    #pdb.set_trace()
     for i in range(0, len(cityinfo['City Code'])):
         if int(cityinfo['City Code'][i]) in cities:
             cities[int(cityinfo['City Code'][i])] = cityinfo[2020][i]
             print("adding info for city {0}".format(cityinfo['City Code'][i]))
 
-    with open('cityinfo_shadow.json', 'w') as jsonfile:
+    with open('cityinfo_shadow.json', 'w') as jsonfile: 
         json.dump(cities, jsonfile)
-
+    with open('pmatrix_shadow.json', 'w') as jsonfile:
+        json.dump(pmatrix, jsonfile)
 
 if __name__ == "__main__":
     sys.exit(main(parser.parse_args()))
