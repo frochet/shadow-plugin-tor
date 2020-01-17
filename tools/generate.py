9a11b07aca72358bc66d259f76a6e929e6b9ea42
Florentin Rochet <florentin.rochet@uclouvain.be>
2020-01-17 13:12:44 +0100

    Bump up tgen client traffic to 2 MiB

--- tools/generate.py
+++ tools/generate.py
@@ -1200,7 +1200,7 @@ def generate_tgen_filetransfer_clients(servers):
     G = DiGraph()
 
     G.add_node("start", socksproxy="localhost:9000", serverport="8888", peers=servers)
-    G.add_node("transfer", type="get", protocol="tcp", size="320 KiB")
+    G.add_node("transfer", type="get", protocol="tcp", size="2 MiB")
     G.add_node("pause", time="1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60")
 
     G.add_edge("start", "transfer")
